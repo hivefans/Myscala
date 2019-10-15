a2c0c24e65ac9db9f16ca1fd464eaf7e0aa85ed5
hivefans <shidongjie@gmail.com>
2019-10-16 00:02:58 +0800

    java and scala

---
+++ src/main/java/es/sdj/hellojava.java
@@ -0,0 +1,8 @@
+package es.sdj;
+
+public class hellojava {
+    public static void main(String[] args) {
+        hello h = new hello();
+        h.sayhello("world");
+    }
+}
---
+++ src/main/scala/es/sdj/hello.scala
@@ -0,0 +1,8 @@
+package es.sdj
+
+class hello {
+  def sayhello(x:String): Unit ={
+    println("Hello scala " + x)
+  }
+
+}
